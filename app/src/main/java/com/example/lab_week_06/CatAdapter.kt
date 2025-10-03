b2e5fa49d3b9ac1b7e773f33f6500758981eeced
Alief987 - Alif <alief987@protonmail.com>
2025-10-03 17:57:01 +0700

    Commit No.2 - Clickable RecyclerView

--- app/src/main/java/com/example/lab_week_06/CatAdapter.kt
+++ app/src/main/java/com/example/lab_week_06/CatAdapter.kt
@@ -6,32 +6,33 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.lab_week_06.model.CatModel
 
 
-class CatAdapter(
-    private val layoutInflater: LayoutInflater,
-    private val imageLoader: ImageLoader
-) : RecyclerView.Adapter<CatViewHolder>() {
-
-    // Mutable list for storing all the list data
+class CatAdapter(private val layoutInflater: LayoutInflater, private val imageLoader: ImageLoader, private val onClickListener:
+                 OnClickListener) : RecyclerView.Adapter<CatViewHolder>() {
+    //Mutable list for storing all the list data
     private val cats = mutableListOf<CatModel>()
-
-    // A function to set the mutable list
+    //A function to set the mutable list
     fun setData(newCats: List<CatModel>) {
         cats.clear()
         cats.addAll(newCats)
-        // Notify that the data has changed
-        notifyDataSetChanged()
+                notifyDataSetChanged()
     }
-    // Instantiate the view holder
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CatViewHolder {
+    //A view holder is used to bind the data to the layout views
+//onCreateViewHolder is instantiating the view holder it self
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
+            CatViewHolder {
         val view = layoutInflater.inflate(R.layout.item_list, parent, false)
-        return CatViewHolder(view, imageLoader)
+        return CatViewHolder(view, imageLoader, onClickListener)
     }
-
-    // Get the number of items in the list
+    //This is used to get the amount of data/item in the list
     override fun getItemCount() = cats.size
-
-    // Bind each data item to the layout views
+    //This is used to bind each data to each layout views
     override fun onBindViewHolder(holder: CatViewHolder, position: Int) {
+//The holder parameter stores our previously created ViewHolder
+//The holder.bindData function is declared in the CatViewHolder
         holder.bindData(cats[position])
+    }
+    //Declare an onClickListener interface
+    interface OnClickListener {
+        fun onItemClick(cat: CatModel)
     }
 }
